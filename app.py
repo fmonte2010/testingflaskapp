0a822087eda25210dd6a4932e1f06000152debff
FaustoMonte <fausto_montenegro@hotmail.com>
2020-12-11 12:11:21 -0500

    Segundo commit

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
+    return 'Esta es una prueba de como se hace deploy de una app Llask en Heroku'
 
 if __name__ == '__main__':
     app.run()
